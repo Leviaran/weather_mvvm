c90aaa950a1cb7b83e36797dd3c4c17be476de75
leviaran <randy.arba@gmail.com>
2018-05-18 03:24:34 +0700

    initialize project

---
+++ app/src/main/java/ran/singletondev/gojek_android_asignment/MainActivity.kt
@@ -0,0 +1,12 @@
+package ran.singletondev.gojek_android_asignment
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
