d0b06b26b26ded598b80b3fae44706e47fffde7e
leviaran <randy.arba@gmail.com>
2018-05-18 13:50:34 +0700

    implement design

--- app/src/main/java/ran/singletondev/gojek_android_asignment/MainActivity.kt
+++ app/src/main/java/ran/singletondev/gojek_android_asignment/MainActivity.kt
@@ -2,11 +2,39 @@ package ran.singletondev.gojek_android_asignment
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.res.ResourcesCompat
+import android.widget.TextView
+import butterknife.BindView
+import butterknife.ButterKnife
 
 class MainActivity : AppCompatActivity() {
+
+    @BindView(R.id.error_text)
+    lateinit var textError : TextView
+
+    @BindView(R.id.big_temp)
+    lateinit var textBigTemp : TextView
+
+    @BindView(R.id.location)
+    lateinit var textLocation : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        ButterKnife.bind(this)
+
+        var thinTypeface = ResourcesCompat.getFont(this, R.font.roboto_thin)
+        var regularTypeface = ResourcesCompat.getFont(this, R.font.roboto_regular)
+        var blackTypeface = ResourcesCompat.getFont(this, R.font.roboto_black)
+
+        textError.typeface = thinTypeface
+
+        textBigTemp.typeface = blackTypeface
+
+        textLocation.typeface = thinTypeface
+
+
+
     }
 }
